6982817dff0bf392d8182dcfdc43603e5f73041b
NamitKumar16 <kumarnamit58@gmail.com>
2022-03-11 23:18:25 +0530

    Update slider.dart

--- lib/widgets/slider.dart
+++ lib/widgets/slider.dart
@@ -9,14 +9,14 @@ class MySlider extends StatelessWidget {
   Widget build(BuildContext context) {
     print("Value Rec From Parent  $value");
     return Container(
-        child: Slider(
-          value :value.toDouble(), // current value
-          min: 1,
-          max:12,
-          onChanged: (double value){
-            fn(value.toInt());
-          },
-        ),
+      child: Slider(
+        value: value.toDouble(), // current value
+        min: 1,
+        max: 12,
+        onChanged: (double value) {
+          fn(value.toInt());
+        },
+      ),
     );
   }
 }
